df7dabe683ff47d9cd8500c6bb66fc3dbfa9c5b0
JesusM15 <guerrajesusm72@gmail.com>
2025-05-02 16:12:59 -0700

    movimiento

--- flappybird/main.c
+++ flappybird/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "raylib.h"
+#include <direct.h>           // _getcwd
 
 int sumar(int a, int b) {
 	int result;
@@ -12,22 +13,46 @@ int sumar(int a, int b) {
 }
 
 int main() {
-	int a = 5, b = 4;
-	int resultado = sumar(a, b);
-	printf("(%d + %d), %d",a,b,resultado);
-
 	InitWindow(900, 600, "Flappy Bird");
+	Texture2D bird = LoadTexture("debug/bird.png");
+	Texture2D cloud1 = LoadTexture("debug/cloud.png");
+	Texture2D cloud2 = LoadTexture("debug/cloud.png");
+	Texture2D cloud3 = LoadTexture("debug/cloud.png");
+	Texture2D cloud4 = LoadTexture("debug/cloud.png");
+	
+	int birdPosY = 200;
 
-	SetTargetFPS(144);
+	SetTargetFPS(40);
 
 	while (!WindowShouldClose()) {
 		BeginDrawing();
-		
-			ClearBackground(RAYWHITE);
-			DrawText("HELLO WORLD!", 320, 200, 20, DARKGRAY);
+
+		ClearBackground(SKYBLUE);
+
+		DrawTexture(cloud1, 40, 30, RAYWHITE);
+		DrawTexture(cloud2, 300, 40, RAYWHITE);
+		DrawTexture(cloud3, 560, 30, RAYWHITE);
+		DrawTexture(cloud4, 800, 10, RAYWHITE);
+
+		DrawTexture(bird, 100, birdPosY, RAYWHITE);
+
+		__asm {
+			MOV eax, birdPosY
+			INC eax
+			MOV birdPosY, eax
+		}
+
+		if (IsKeyPressed(KEY_SPACE)) {
+			__asm {
+				MOV eax, birdPosY
+				SUB eax, 40
+				MOV birdPosY, eax
+			}
+		}
 		EndDrawing();
 	}
 
 	CloseWindow();
+	
 	return 0;
 }
