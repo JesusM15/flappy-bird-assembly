a6fe7ee2b9091f888f4f2056586159ba07e4bfbf
JesusM15 <guerrajesusm72@gmail.com>
2025-05-02 10:16:46 -0700

    Agregar archivos de proyecto.

---
+++ flappybird/main.asm
@@ -0,0 +1,13 @@
+.386 
+.model flat, stdcall
+.stack 4096
+
+.DATA ;todo las variables a usar en el programa se declaran debajo de esta directivo
+
+.CODE; para colocar las instrucciones abajo de esta directiva
+main PROC
+	
+	
+	RET
+main ENDP ;terminamos el procedimiento
+END main ;terminamos el main
